d136ba6164eacfc49e1f4844bd6586b66d4b771c
= <=>
2025-11-20 13:45:27 +0100

    Make* functions for creating graph nodes and conns

--- Assets/Scripts/NNGraphMaker.cs
+++ Assets/Scripts/NNGraphMaker.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -22,22 +21,45 @@ public class NNGraphMaker : MonoBehaviour
 
     public bool MakeGraph(List<ConnectionGenome> c, List<NodeGenome> n, List<NodeGenome> inn)
     {
+        
+        MakeConnection(MakeInNode(Vector3.zero), MakeOutNode(Vector3.zero + new Vector3(100, 0, 0)));
+        return true; //success
+    }
+
+    Vector3 MakeNode(Vector3 pos)
+    {
+        GameObject NodeGO = Instantiate(nodePrefab);
+        NodeGO.transform.SetParent(transform, true);
+        NodeGO.transform.localPosition = pos;
+        return NodeGO.transform.position;
+    }
+
+    Vector3 MakeInNode(Vector3 pos)
+    {
         GameObject inNodeGO = Instantiate(inNodePrefab);
         inNodeGO.transform.SetParent(transform, true);
-        inNodeGO.transform.localPosition = Vector3.zero;
+        inNodeGO.transform.localPosition = pos;
+        return inNodeGO.transform.position;
+    }
 
+    Vector3 MakeOutNode(Vector3 pos)
+    {
         GameObject outNodeGO = Instantiate(outNodePrefab);
         outNodeGO.transform.SetParent(transform, true);
-        outNodeGO.transform.localPosition = Vector3.zero + new Vector3(100, 0, 0);
+        outNodeGO.transform.localPosition = pos;
+        return outNodeGO.transform.position;
+    }
 
+    void MakeConnection(Vector3 inPos, Vector3 outPos)
+    {
         GameObject go = new GameObject("RuntimeLine");
         LineRenderer lr = go.AddComponent<LineRenderer>();
         go.transform.SetParent(transform);
 
         // Základní nastavení
         lr.positionCount = 2;
-        lr.SetPosition(0, inNodeGO.transform.position);
-        lr.SetPosition(1, outNodeGO.transform.position);
+        lr.SetPosition(0, inPos);
+        lr.SetPosition(1, outPos);
         lr.useWorldSpace = true;
         lr.widthMultiplier = 0.05f;
 
@@ -52,6 +74,5 @@ public class NNGraphMaker : MonoBehaviour
         lr.endWidth = 1;
 
         lr.sortingOrder = 0;
-        return true; //success
     }
 }
